86e2e43cdb9fb5edc8eefce31e91012b439c5987
Pankaj Bisht <pankajbishtuk04@gmail.com>
2026-06-24 21:37:13 +0530

    promise-methods-polyfill

--- Week_01/polyFills.js
+++ Week_01/polyFills.js
@@ -28,7 +28,7 @@ Array.prototype.myForEach = function (cb) {
 //while calling arr.myMap js
 // console.log(forEachs)
 
-//Promise.all takes an array of Promises and returns a array . It resolves if all Promises gets resolved and rejected as soon as one of the Promise got rejected.
+// ✨ Promise.all takes an array of Promises and returns a array . It resolves if all Promises gets resolved and rejected as soon as one of the Promise got rejected.
 
 
 const p1 = Promise.resolve("1");
@@ -61,3 +61,58 @@ output.then(data => {
 }).catch(err=> {
     console.log("rejected with" + err)
 }) 
+
+//Promise.allSettled() is a promise method that takes an array of promises and returns array of promises.
+//Promise.allSettled() will wait for all promises to settle (be it resolved and rejected) and returns an array of objects containing each promise's status and result.;
+
+
+function myPromiseAllSettled(promiseArray){
+    let status = []
+    let completedPromiseCount  = 0
+    return new Promise((resolve, reject) => {
+            for(let i = 0; i < promiseArray.length; i++){
+                promiseArray[i].then((data) => {
+                    status[i] = {
+                        status:"resolved",
+                        data
+                    } 
+                    //preserves the order as well
+
+
+                }).catch((reason) => {
+                    status[i] = {
+                        status:"rejected",
+                        reason
+                    }
+                    //preserves the order as well
+
+
+                }).finally(() => {
+                    completedPromiseCount++;
+                    if(completedPromiseCount === promiseArray.length){
+                        resolve(status)
+                    }
+                    
+
+                })
+            }
+
+    })
+}
+
+
+// 🚀Promise.race() takes array of promises and returns the value (not array) of first settled promise be it resolved or rejected
+function myPromiseRace(promiseArray) {
+    return new Promise((resolve, reject) => {
+        for(let i = 0; i < promiseArray.length; i++){
+            promiseArray[i].then((data) => {
+                resolve(data)
+
+            }).catch((err) => {
+                reject(err)
+
+            })
+        }
+    })
+    
+}
