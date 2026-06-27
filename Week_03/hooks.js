45a4c07f0a7acd8672ed91e5955ede5d4192265a
Pankaj Bisht <pankajbishtuk04@gmail.com>
2026-06-27 22:39:39 +0530

    practice

---
+++ Week_03/hooks.js
@@ -0,0 +1,8 @@
+//Hooks are utility functions that are given to us by react to store a value and perform lifecycle logic in functional components.
+//💥useMemo is a react hook or function that takes a callback function and dependency array,whenever the depedency array changes the callback function gets executed
+// useMemo is used to memoize a computational value inside functional component
+
+// Why useMemo => 1. Every re-render runs the component function so the expensive calculations/ functions runs too
+
+
+
